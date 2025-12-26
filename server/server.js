dfc0d9b1b001040754a66368b7c69fbdfd59e2bc
SaiAshwathaSingari <singarisai777@gamil.com>
2025-12-27 04:46:02 +0530

    Resume Model

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config"
 import connectDB from "./lib/db.js";
+import userRouter from "./Routes/AppRoutes.js";
+
 const app = express();
 app.use(express.json())
 app.use(cors())
@@ -12,7 +14,7 @@ connectDB();
 
 
 
-
+app.use('/api/users',userRouter);
 
 app.listen(port,()=>{
   console.log(`Server is running on http://localhost:3000`)
