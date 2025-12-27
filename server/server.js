87d5fc8c25d3d9835dc4d91adb172a4b7b377096
SaiAshwathaSingari <singarisai777@gamil.com>
2025-12-28 01:33:26 +0530

    Resume Controller and Routes

--- server/server.js
+++ server/server.js
@@ -2,7 +2,8 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config"
 import connectDB from "./lib/db.js";
-import userRouter from "./Routes/AppRoutes.js";
+import userRouter from "./Routes/UserRoutes.js";
+import resumeRotuer from "./Routes/ResumeRoutes.js";
 
 const app = express();
 app.use(express.json())
@@ -15,7 +16,7 @@ connectDB();
 
 
 app.use('/api/users',userRouter);
-
+app.use('/api/resumes',resumeRotuer);
 app.listen(port,()=>{
   console.log(`Server is running on http://localhost:3000`)
 })
